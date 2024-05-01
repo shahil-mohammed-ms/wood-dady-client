c1310b5847508c9575a7a1f9d7dccc50e66d42e3
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2024-05-01 20:38:33 +0530

    update

--- src/components/historySec/HistorySec.jsx
+++ src/components/historySec/HistorySec.jsx
@@ -23,7 +23,7 @@ function HistorySec() {
            the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
           The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
     </div>
-    <img src="/gallery/a1.jpg" alt="" />
+    <img src="/gallery/a2.jpg" alt="" style={{height:'600px'}} />
   </div>
   )
 }
